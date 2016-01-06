ce9ae9759f4f2bc6a7c7904d74f29ddd7292bad0
J Hyde <jkhyde86@gmail.com>
2016-01-06 14:36:53 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
-  get 'static_pages/about'
+  #get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 end
