30d9f4dca23c2cb706f7c19be944608baed76bdb
J Hyde <jkhyde86@gmail.com>
2016-01-25 16:25:57 -0500

    Rebuilt main page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   before_action :check_admin, only: [:admin]
 
   def home
+    @clays = ClayShipment.count
+    @mines = 0
+    @towers = 0
   end
 
   def about
@@ -9,4 +12,26 @@ class StaticPagesController < ApplicationController
 
   def admin
   end
+
+  def report
+  	@error = NewsItem.new
+  end
+
+  def submit
+  	@message = NewsItem.new(error_params)
+  	@message.users << User.where(admin: true)
+  	@message.msg_type = "error_report"
+  	if @message.save
+  		flash[:success] = "Thank you for your report"
+  		redirect_to '/'
+  	else
+  		flash[:danger] = "There was an error with the error reporting. That's just fucked up."
+  		render 'report'
+  	end
+  end
+
+  private
+  	def error_params
+  		params.require(:news_item).permit(:message)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   #get 'static_pages/about'
   get 'about' => 'static_pages#about'
   get 'admin' => 'static_pages#admin'
+  get 'report' => 'static_pages#report'
+  post 'submit' => 'static_pages#submit'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
