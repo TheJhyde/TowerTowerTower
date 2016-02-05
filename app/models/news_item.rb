c9270afd910b0411f1046f2db762c83fc23a71c6
J Hyde <jkhyde86@gmail.com>
2016-02-05 18:00:19 -0500

    Added tell all function

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,4 +1,12 @@
 class NewsItem < ActiveRecord::Base
 	has_and_belongs_to_many :users
 	validates :message, presence: true
+
+	def self.tell_all(news)
+		 broadcast = NewsItem.create(msg_type: "news", message: news)
+		 User.all.each do |user|
+		 	user.news_items << broadcast
+		 	user.save
+		 end
+	end
 end
