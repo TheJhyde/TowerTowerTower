2023f0577c9a8f1a513baed9455ae8983cba8936
J Hyde <jkhyde86@gmail.com>
2016-02-05 17:44:50 -0500

    New seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,15 +25,15 @@ end
 words.uniq!
 
 urls.each_with_index do |url, i|
-	Glyph.create(url: url, meaning: words[i])
+	#Glyph.create(url: url, meaning: words[i])
 end
 
-NewsItem.create(msg_type: "new", message: "Welcome to Tower Brick Lightning! To get updates 
-	about the game, check out our <a href ='http://towerbricklightning.tumblr.com/'>tumblr</a>
-	 or <a href='https://twitter.com/TBLGame'>twitter</a>.")
+# NewsItem.create(msg_type: "new", message: "Welcome to Tower Brick Lightning! To get updates 
+# 	about the game, check out our <a href ='http://towerbricklightning.tumblr.com/'>tumblr</a>
+# 	 or <a href='https://twitter.com/TBLGame'>twitter</a>.")
 
 5.times do |i|
-	5.times do |j|
+	2.times do |j|
 		Brick.create(x: i, y: j, color: rand(3))
 	end
 end
