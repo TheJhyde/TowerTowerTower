ad64ca77a93a1f0e870dd1926eea7a24f5d673bd
J Hyde <jkhyde86@gmail.com>
2016-02-17 17:19:25 -0500

    Password reset, minus the tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  #get 'static_pages/about'
   get 'about' => 'static_pages#about'
   get 'admin' => 'static_pages#admin'
   get 'report' => 'static_pages#report'
@@ -13,7 +12,8 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :build_orders, only: [:new, :create, :index]
+  get 'build_orders/finished' => 'build_orders#finished'
   resources :tower, only: [:index]
   resources :tower_pics, only: [:show, :index]
-  get 'build_orders/finished' => 'build_orders#finished'
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211181205) do
+ActiveRecord::Schema.define(version: 20160217214133) do
 
   create_table "bricks", force: :cascade do |t|
     t.integer  "x"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 20160211181205) do
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.integer  "actions",           default: 2
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
