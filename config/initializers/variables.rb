d9f1ccfb45eb36104886e5092881ca28d0a3832e
J Hyde <jkhyde86@gmail.com>
2016-02-19 20:27:55 -0500

    Updated messages

--- config/initializers/variables.rb
+++ config/initializers/variables.rb
@@ -5,9 +5,10 @@ Tbl::Application.config.x.glyph_url = "https://s3.amazonaws.com/towerbricklightn
 Tbl::Application.config.x.img_url = "https://s3.amazonaws.com/towerbricklightning/"
 
 #Player variables------------------------------------------------------------------------------
-Tbl::Application.config.x.max_actions = 9
-Tbl::Application.config.x.daily_actions = 2
-Tbl::Application.config.x.starting_actions = 3
+Tbl::Application.config.x.max_actions = 20
+Tbl::Application.config.x.daily_actions = 6
+Tbl::Application.config.x.starting_actions = 6
+Tbl::Application.config.x.stranger_actions = 3 #how many times people not logged in can play per day
 
 #Clay types
 Tbl::Application.config.x.clay_types = 3 #How many kinds of clay there are
@@ -25,10 +26,11 @@ Tbl::Application.config.x.bricks_layer = 10 #How many bricks there are in each l
 Tbl::Application.config.x.screen_height = 500
 Tbl::Application.config.x.screen_width = 
 	Rails.configuration.x.brick_width * (Rails.configuration.x.bricks_layer + 2)
+
 #How many bricks there are on each level
 Tbl::Application.config.x.level_height = 
 	(Rails.configuration.x.screen_height/Rails.configuration.x.brick_height).floor + 1
 
-Tbl::Application.config.x.shipment = 2 #Players get this many bricks in each shipment
+Tbl::Application.config.x.shipment = 1 #Players get this many bricks in each shipment
 
 Tbl::Application.config.x.max_levels = 10
