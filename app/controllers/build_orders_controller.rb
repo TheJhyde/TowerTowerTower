f2bfb34c22e49fff6c42a03fff9fb6a86f44f329
J Hyde <jkhyde86@gmail.com>
2016-02-25 13:28:33 -0500

    Small fixes

--- app/controllers/build_orders_controller.rb
+++ app/controllers/build_orders_controller.rb
@@ -16,11 +16,17 @@ class BuildOrdersController < ApplicationController
 	end
 
 	def create
+		if params["build_order"]["message"].empty?
+			flash[:danger] = "Bricks must have a message."
+			redirect_to new_build_order_path and return
+		end
+
 		@order = BuildOrder.new
 		@order.message = params["build_order"]["message"]
 		@order.x = params["build_order"]["x"].split(" ").map{|x| x.to_i}
 		@order.y = params["build_order"]["y"].split(" ").map{|x| x.to_i}
 		@order.colors = params["build_order"]["colors"].split(" ").map{|x| x.to_i}
+
 		if logged_in?
 			@order.user = current_user
 		end
@@ -28,9 +34,9 @@ class BuildOrdersController < ApplicationController
 		if @order.save
 			if logged_in?
 				current_user.update(actions: current_user.actions - 1)
-				flash[:success] = "You have placed your bricks! Your order will be resolved at #{(DateTime.now + 1.hour).strftime("%l %p")}. You have #{current_user.actions} actions left for the day."
+				flash[:success] = "Bricks placed. Your order will be resolved at #{(DateTime.now + 1.hour).strftime("%l %p")}. You have #{current_user.actions} actions left for the day."
 			else
-				flash[:success] = "Bricks placed! Your order will be resolved at #{(DateTime.now + 1.hour).strftime("%l %p")}. Want to see what happened to your bricks? Sign up to find out!"
+				flash[:success] = "Bricks placed. Your order will be resolved at #{(DateTime.now + 1.hour).strftime("%l %p")}. Sign up to see what happened to your bricks."
 				session["acted"] = session["acted"] + 1;
 				session["build_order"] = @order.id
 			end
