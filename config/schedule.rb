050e035152fca2049a78addef1823947a18e2001
J Hyde <jkhyde86@gmail.com>
2016-03-20 16:09:21 -0700

    Test on git, try 2

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,37 +1,4 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-set :environment, "development"
-
-# every :hour do
-# 	rake "hourly_tasks"
-# end
-
-# every :day do
-# 	rake "daily_tasks"
-# end
-
-# every 1.hour, :at => "30" do
-# 	rake "hourly_tasks"
-# end
+set :environment, "production"
 
 every '*/5 * * * *' do
   rake 'place_bricks'
