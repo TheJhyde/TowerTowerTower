a2679315e9c6596491ed146eac633ff5ae060a61
J Hyde <jkhyde86@gmail.com>
2016-03-24 22:55:24 -0400

    Turned time back on

--- config/clock.rb
+++ config/clock.rb
@@ -8,6 +8,6 @@ module Clockwork
   end
 
   every(1.hour, 'Run Build Orders', tz: 'EST', :at => ['**:00', '**:05', '**:10', '**:15', '**:20', '**:25', '**:30', '**:35', '**:40', '**:45', '**:50', '**:55']) { BuildOrder.resolve_orders }
-  #every(1.day, 'Update actions', :at => '12:00', tz: 'EST') {User.daily_tasks}
-  every(1.hour, 'Update actions', :at => '**:00', tz: 'EST') {User.daily_tasks}
+  every(1.day, 'Update actions', :at => '12:00', tz: 'EST') {User.daily_tasks}
+  #every(1.hour, 'Update actions', :at => '**:00', tz: 'EST') {User.daily_tasks}
 end
