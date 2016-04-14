3383f4d64bbae010f45cae767a2130390f7bf2b6
J Hyde <jkhyde86@gmail.com>
2016-04-14 16:44:14 -0400

    Fixed email thing, slow_blink tag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     :authentication =>  :plain,
     :user_name =>       ENV['SENDGRID_USERNAME'],
     :password =>        ENV['SENDGRID_PASSWORD'],
-    :domain =>          'towerbricklightning.heroku.com',
+    :domain =>          'ww.towertowertower.top',
     :enable_starttls_auto => true
   }
 
